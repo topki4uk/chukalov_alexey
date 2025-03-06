b358ff3a2e002c7a190c9460242d4c0d93e72e9a
chukalovai <topki4uk@mail.ru>
2025-03-06 23:08:32 +0300

    add aspect test for spring

--- src/main/java/com/example/demo/aspect/LoggingAspect.java
+++ src/main/java/com/example/demo/aspect/LoggingAspect.java
@@ -1,33 +1,34 @@
 package com.example.demo.aspect;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.time.Instant;
-
+@Getter
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
 
-  @Before("execution(* com.example.demo.controller.*(..))")
+  private int counter;
+
+  public void startCounter() {
+    counter = 0;
+  }
+
+  @Before("execution(* com.example.demo.controller.*.*( .. ))")
   public void logBefore(JoinPoint joinPoint) {
+    counter++;
     log.debug("{}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
   }
 
-  @Around("execution(* com.example.demo.controller.*(..))")
-  public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-    Instant start = Instant.now();
-    Object result = joinPoint.proceed();
-    Instant end = Instant.now();
-    String duration = Duration.between(start, end).toMillis() + "ml";
-    log.debug(duration);
-    return result;
+  @After("execution(* com.example.demo.controller.*.*( .. ))")
+  public void logAfter(JoinPoint joinPoint) {
+    counter++;
+    log.debug("{}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
   }
 }
